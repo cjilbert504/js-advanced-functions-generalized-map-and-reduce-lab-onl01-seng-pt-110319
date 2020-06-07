512180eef2677914ae36adf071817c80dada5ccb
Collin Jilbert <cjilbert504@gmail.com>
2020-06-06 23:30:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(numArr, func) {
+    let newArr = [];
+    numArr.forEach(element => {
+        newArr.push(func(element));
+    });
+    return newArr;
+}
+
+// function reduce(sourceArray, func, startingPoint) {
+//     let start = (!!startingPoint) ? startingPoint : 0;
+//     sourceArray.forEach(element => {
+//         start = func(element, start);
+//     })
+//     return start;
+// }
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
